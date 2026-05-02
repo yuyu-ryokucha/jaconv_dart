c0eb958a7791021febb1041792fcb886ed72f999
test <test@example.com>
2026-05-02 10:54:49 +0900

    initial commit

---
+++ bin/jaconv_dart.dart
@@ -0,0 +1,5 @@
+import 'package:jaconv_dart/jaconv_dart.dart' as jaconv_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${jaconv_dart.calculate()}!');
+}
---
+++ test/jaconv_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:jaconv_dart/jaconv_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
